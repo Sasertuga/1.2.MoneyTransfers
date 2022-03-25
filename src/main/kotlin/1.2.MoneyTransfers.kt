a8fb24f5d57c5a911e1a50cd7b88af5f567f2be4
Yarygin Maksim <goprobeaheroo@vk.com>
2022-03-25 08:24:36 +0500

    rework

--- src/main/kotlin/1.2.MoneyTransfers.kt
+++ src/main/kotlin/1.2.MoneyTransfers.kt
@@ -1,10 +1,11 @@
-const val  COMMISSION = 0.75
+const val COMMISSION = 0.75
 
 fun main() {
-    println("Введите сумму перевода в рублях но не меньше 35 рублей: ")
+    println("Введите сумму перевода в рублях: ")
     val input = readLine()?.toInt() ?: return
-    val amount: Int = if (input >= 35) input * 100 else "Сумма меньше 35 рублей".toInt()
+    val amount = if (input !== 0 && input > 0) input * 100 else return
     val commissionForTransfer = (amount * COMMISSION / 100).toInt()
+    val lol = if (commissionForTransfer < 3500 ) 35 * 100 else commissionForTransfer
 
-    print("Комиссия за перевод: $commissionForTransfer копеек")
+    print("Комиссия за перевод: $lol копеек")
 }
